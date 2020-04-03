7dc1e9f59736eacec30023821302e17702004a42
nortongoncalves <goncalvespnorton@gmail.com>
2020-04-02 21:17:10 -0300

    melhorando o server e o cliente

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const serverTCP = require('./serverTCP');
+const port = 3030;
+const host = 'localhost';
+serverTCP(port,host);
--- src/serverTCP.js
+++ src/serverTCP.js
@@ -1,28 +1,41 @@
-const net = require('net');
-function serverTCP(port,host){
-    const server = net.createServer();
+const net = require('net'); // importando o modulo net do node
+function findIndexClient(sockets,socket) { // função que obtem a posição do usuario no array sockets
+    let index = sockets.findIndex(ress => {
+        return ress.remoteAddress === socket.remoteAddress && ress.remotePort === socket.remotePort;
+    });
+    return index;
+}
+function mountData(sockets,index,data) { // monta o retorno dos dados do cliente
+    const { remoteAddress } = sockets[index];
+    const { remotePort } = sockets[index];
+    console.log(`Client [${remoteAddress}:${remotePort}]: ${String(data)}`);
+}
+function serverTCP(port,host){ 
+    const server = net.createServer(); // cria o server e salva na variavel server
     let sockets = [];
     
-    server.listen(port);
+    server.listen(port,host); // escuta na porta e host colocados na função
 
-    server.on('connection', socket => {
-        console.log(`Client Connected => ${socket.remoteAddress}:${socket.remotePort}`);
-        sockets.push(socket);
+    server.on('connection', socket => { // monitora o evento de connection de um cliente que quando conectado executa uma função de callback
+        console.log(`Client Connected => ${socket.remoteAddress}:${socket.remotePort} \n`); 
+        sockets.push(socket); // coloca o socket do cliente no array de sockets
 
-        socket.on('data', data => {
-            console.log('RECEIVED DATA \n');
-            console.log(data);
-            console.log('\n');
+        socket.on('data', data => { // monitora o evento de data de quando o servidor recebe alguma mensagem 
+            const index = findIndexClient(sockets,socket); 
+            mountData(sockets,index,data);
         });
 
-        socket.on('close', data => {
-            let client = sockets.findIndex(ress => {
-                return ress.remoteAddress === socket.remoteAddress && ress.remotePort === socket.remotePort;
-            });
+        socket.on('close', data => { // monitora o evento de close quando um cliente se desconecta do servidor
+            const index = findIndexClient(sockets,socket); 
 
-            if(client !== -1) sockets.splice(client, 1);
+            if(index !== -1) sockets.splice(index, 1); // exclui a informação do socket do usuario que se desconectou no array sockets
             console.log(`CLOSED: ${socket.remoteAddress}:${socket.remotePort}`);
         });
+
+        socket.on('error', err => { // monitora o evento de erro 
+            console.error(`cliente se desconectou abruptamente ${err}`);
+        });
     });
+    console.log(`server: ${host}:${port}`);
 }
-module.exports = serverTCP;
+module.exports = serverTCP; // exporta o módulo para que seja possível acessar em outro módulo 
